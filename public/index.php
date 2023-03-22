d2e5a9229b06efbfe43f47760a61a97e1e114b4c
Ilya <devdokimov.ilya@yandex.ru>
2023-03-23 00:38:41 +0300

    Определение контроллера и метода

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-Hello world!
+<?php
+
+//Определяем контроллер по get параметру
+$controllerName = isset($_GET["controller"]) ? ucfirst($_GET["controller"]) : "Index";
+//Определяем метод по get параметру
+$actionName = isset($_GET["action"]) ? $_GET["action"] : "index";
+
+echo "Controller: ". $controllerName , "<br />Action: ". $actionName;
