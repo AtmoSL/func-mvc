9927b281a568db4bdc2e9037de388fda9820203e
Ilya <devdokimov.ilya@yandex.ru>
2023-03-26 01:19:40 +0300

    добавление вёрстки

---
+++ views/footer.php
@@ -0,0 +1,12 @@
+        </main>
+            <footer class="footer">
+                <div class="container">
+                    <div class="footer__text">
+                        Разработано  специально для <a href="#">портфолио</a>
+                    </div>
+                </div>
+            </footer>
+        <!--конец wrapper-->
+        </div>
+    </body>
+</html>
--- views/index.php
+++ views/index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>func-mvc</title>
-</head>
-<body>
+<?php require_once 'head.php'; ?>
+
     <?php echo $hello; ?>
-</body>
-</html>
+
+<?php require_once 'footer.php'; ?>
