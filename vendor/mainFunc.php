38053be74cc0f5d767865cdbddf7473df55a119b
Ilya <devdokimov.ilya@yandex.ru>
2023-04-04 15:18:36 +0300

    Проверка существования контроллера перед запуском

--- vendor/mainFunc.php
+++ vendor/mainFunc.php
@@ -7,15 +7,32 @@
  * @param String $controllerName
  * @param String $actionName
  */
-function load($controllerName, $actionName){
+function load($controllerName, $actionName)
+{
+    if(!file_exists(ControllersPrefix . $controllerName . ControllersPostfix)) header("location: /");
     //Подключаем контроллер
     include_once ControllersPrefix . $controllerName . ControllersPostfix;
     //Определяем метод
-    $action = $actionName."Action";
+    $action = $actionName . "Action";
 
     //Вызываем метод
     $action();
 
+}
+
+/**
+ *
+ * Функция дебага
+ *
+ * @param $value
+ * @param $die
+ */
+function debug($value = null, $die = 1)
+{
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+    if ($die) die;
 }
 
 
@@ -26,39 +43,58 @@ function load($controllerName, $actionName){
  * @param array $params
  * @return void
  */
-function render($view, $params = []){
+function render($view, $params = [])
+{
     extract($params);
-    require_once "../views/".$view.".php";
+    require_once "../views/" . $view . ".php";
 }
 
+
+//TODO Вынести функции для БД в отдельный файл
+//TODO Сделать функцию для получения 1 строчки в виде массива
 /**
  * Преобразование результата SQL в массив
  *
  * @param $sql
  * @return array
  */
-function mysqliQueryArray($sql){
+function mysqliQueryArray($sql)
+{
 
     $row = mysqli_query(DB_CONNECT, $sql);
 
     $result = [];
 
-    while($rw = $row->fetch_assoc()){
+    while ($rw = $row->fetch_assoc()) {
         $result[] = $rw;
     }
     return $result;
 }
 
 /**
+ *  Создание строчки в БД
  *
- * Функция дебага
- *
- * @param $value
- * @param $die
+ * @param $sql
+ * @return bool
  */
-function debug($value = null, $die = 1){
-    echo '<pre>';
-    print_r($value);
-    echo '</pre>';
-    if($die) die;
+function mysqliCreate($sql)
+{
+    mysqli_query(DB_CONNECT, $sql);
+
+    return true;
+}
+
+function mysqliRowCheck($conditions, $tableName)
+{
+    $conditionRow = "";
+    foreach ($conditions as $key => $condition){
+        $conditionRow .= "$key = '$condition'";
+    }
+
+    $sql = "SELECT * FROM `$tableName` WHERE $conditionRow";
+    $rows = mysqli_query(DB_CONNECT, $sql);
+
+    $rowsArray = mysqli_fetch_array($rows);
+    if ($rowsArray) return true;
+    return false;
 }
