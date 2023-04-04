4d2c7c61a75725902cdb30fac066de54bd1421ab
Ilya <devdokimov.ilya@yandex.ru>
2023-04-04 23:58:41 +0300

    Поправил стили

--- views/register.php
+++ views/register.php
@@ -2,25 +2,25 @@
 
 <section class="registration">
     <div class="container">
-        <form class="registration__form" action="/register/registration/" method="post">
-            <div class="registration__form__element">
+        <form class="registration__form form" action="/register/registration/" method="post">
+            <div class="registration__form__element form__element">
                 <label for="fullName">ФИО</label>
                 <input type="text" name="fullName" id="fullName">
             </div>
-            <div class="registration__form__element">
+            <div class="registration__form__element form__element">
                 <label for="email">Почта</label>
                 <input type="text" name="email" id="email">
             </div>
-            <div class="registration__form__element">
+            <div class="registration__form__element form__element">
                 <label for="password">Пароль</label>
                 <input type="password" name="password" id="password">
             </div>
-            <div class="registration__form__element">
+            <div class="registration__form__element form__element">
                 <label for="password__repeat">Подтверждение пароля</label>
                 <input type="password" name="passwordRepeat" id="passwordRepeat">
             </div>
-            <div class="registration__form__element">
-                <button type="submit" class="registration__form__btn">Зарегистрироваться</button>
+            <div class="registration__form__element form__element">
+                <button type="submit" class="registration__form__btn form__btn">Зарегистрироваться</button>
             </div>
         </form>
     </div>
